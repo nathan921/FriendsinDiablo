2d97471ae3e5d614be69bbf3801a3125d31ebbbb
Nathan <nathan921@gmail.com>
2015-02-24 16:34:46 -0700

    Completed RecyclerView with Adapter
    Need to implement an onClickListener for the new RecyclerView

--- app/src/main/java/com/ddtpt/friendsindiablo/FragmentCareerList.java
+++ app/src/main/java/com/ddtpt/friendsindiablo/FragmentCareerList.java
@@ -1,6 +1,6 @@
 package com.ddtpt.friendsindiablo;
 
-import android.app.ListFragment;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -29,21 +29,17 @@ import retrofit.converter.GsonConverter;
 /**
  * Created by e228596 on 2/19/2015.
  */
-public class FragmentCareerList extends ListFragment {
+public class FragmentCareerList extends Fragment {
     private ArrayList<String> storedCharacters;
     private static final String ENDPOINT = "http://us.battle.net";
     private static final String FILENAME = "careers.json";
     private Utils mUtils;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView recList;
+    private LinearLayoutManager llm;
 
-    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        RecyclerView recList = (RecyclerView)getActivity().findViewById(R.id.careerList);
-        recList.setHasFixedSize(true);
-        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
-        llm.setOrientation(LinearLayoutManager.VERTICAL);
-        recList.setLayoutManager(llm);
 
         //TODO: GET STORED CHARACTER LIST AND FILL THE ARRAYLIST
         mUtils = new Utils(getActivity(), FILENAME);
@@ -51,10 +47,16 @@ public class FragmentCareerList extends ListFragment {
         storedCharacters = new ArrayList<>();
         storedCharacters = mUtils.getCareerList();
 
-        setListAdapter(new CharacterListAdapter(storedCharacters));
+        recList = (RecyclerView)getActivity().findViewById(R.id.careerList);
+        recList.setHasFixedSize(true);
+        llm = new LinearLayoutManager(getActivity());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recList.setLayoutManager(llm);
+
+        mAdapter = new CharacterAdapter(storedCharacters);
+        recList.setAdapter(mAdapter);
+
     }
-
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
@@ -63,15 +65,10 @@ public class FragmentCareerList extends ListFragment {
         return v;
     }
 
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        String selectedHero = getListAdapter().getItem(position).toString();
-    }
-
-    private class CharacterListAdapter extends RecyclerView.Adapter<String> {
+    public class CharacterAdapter extends RecyclerView.Adapter<CareerViewHolder> {
         private ArrayList<String> careerList;
 
-        public CharacterListAdapter(ArrayList<String> chars) {
+        public CharacterAdapter(ArrayList<String> chars) {
             careerList = chars;
         }
 
@@ -81,6 +78,20 @@ public class FragmentCareerList extends ListFragment {
         }
 
         @Override
+        public void onBindViewHolder(CareerViewHolder cvh, int i) {
+            String careerName = careerList.get(i);
+            cvh.vName.setText(careerName);
+        }
+
+        @Override
+        public CareerViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.career_card_layout, viewGroup, false);
+            return new CareerViewHolder(itemView);
+        }
+
+
+
+       /* @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             if (convertView == null) {
                 convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item_character, null);
@@ -91,6 +102,15 @@ public class FragmentCareerList extends ListFragment {
             charNameTextView.setText(character);
 
             return convertView;
+        }*/
+    }
+
+    public static class CareerViewHolder extends RecyclerView.ViewHolder {
+        protected TextView vName;
+
+        public CareerViewHolder(View v) {
+            super(v);
+            vName = (TextView)v.findViewById(R.id.career_name);
         }
     }
 
